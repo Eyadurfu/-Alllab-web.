6c61c41a41e11c5b189ceef59fce7096101bb87d
Eyadurfu <168453196+Eyadurfu@users.noreply.github.com>
2024-04-30 15:32:46 +0500

    Create accordion.js

---
+++ 13.10 lab/Fonts/js/accordion.js
@@ -0,0 +1,12 @@
+$(function () {
+  // Стандартная настройка
+  $(".questions__list").accordion({
+    active: true,
+    collapsible: true,
+    animate: 300,
+    heightStyle: "content"
+  });
+
+  // Персональная настройка
+
+});
